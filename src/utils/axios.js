589e5c895229f63aed19349e72dfcea209495228
muntasir23 <muntasiraahmed3@gmail.com>
2024-09-01 09:25:18 -0700

    render include

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://vanguard-server.onrender.com",
 });
 export default axiosInstance;
